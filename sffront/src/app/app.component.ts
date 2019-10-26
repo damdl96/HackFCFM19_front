b17642edcf452963587f57b92198e53ef05409ed
Genaro Almaraz <jg08950@gmail.com>
2019-10-26 15:34:33 -0500

    Feat: Service Worker versions management added

--- sffront/src/app/app.component.ts
+++ sffront/src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'sffront';
+  constructor(private swUpdate : SwUpdate) {
+
+  }
+  ngOnInit(): void {
+    if(this.swUpdate.isEnabled) {
+      this.swUpdate.available.subscribe(() => {
+        window.location.reload();
+      });
+    }
+  }
 }
