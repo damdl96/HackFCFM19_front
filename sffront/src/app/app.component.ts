e7f768f8c402339aeecd46517f9f34a891324dc9
Genaro Almaraz <jg08950@gmail.com>
2019-10-27 07:24:27 -0600

    Feat: Chart.js implemented and dynamically graphing data.

--- sffront/src/app/app.component.ts
+++ sffront/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { SwUpdate } from '@angular/service-worker';
 import { WebsocketService } from './websocket.service';
+import { Chart } from 'chart.js';
 
 @Component({
   selector: 'app-root',
@@ -12,6 +13,10 @@ export class AppComponent implements OnInit {
   constructor(private swUpdate : SwUpdate, private webSocketService : WebsocketService) {
 
   }
+
+  chartTemperature = [];
+  chartHumidity = [];
+
   ngOnInit(): void {
     if(this.swUpdate.isEnabled) {
       this.swUpdate.available.subscribe(() => {
@@ -19,13 +24,61 @@ export class AppComponent implements OnInit {
       });
     }
 
-    this.webSocketService.listen('http:temp').subscribe((data) => {
+    this.chartTemperature = new Chart('temperature', {
+      type: 'line',
+      options: {
+        responsive: true,
+        title: {
+          display: true,
+          text: 'Temperatura'
+        },
+      },
+      data: {
+        labels: ['Jamón', 'Huevo', 'Carne'],
+        datasets: [{
+            label: 'Mediciones',
+            data: [2430, 15600, 365],
+            backgroundColor: ["pink", "blue", "red"],
+            borderColor: 'black',
+            fill: false,
+          }],
+      }
+    });
+
+    this.chartHumidity = new Chart('humidity', {
+      type: 'bar',
+      options: {
+        responsive: true,
+        title: {
+          display: true,
+          text: 'Humedad'
+        },
+      },
+      data: {
+        labels: ['Jamón', 'Huevo', 'Carne'],
+        datasets: [{
+            label: 'Mediciones',
+            data: [2430, 15600, 365],
+            backgroundColor: ["pink", "blue", "red"],
+            borderColor: 'blue',
+            fill: false,
+          }],
+      }
+    });
+
+    this.webSocketService.listen('mqtt:temp').subscribe((data) => {
       console.log('Temperaturas: ' + JSON.stringify(data));
+      this.updateChartData(this.chartTemperature, data, 0);
     })
 
-    this.webSocketService.listen('http:hum').subscribe((data) => {
+    this.webSocketService.listen('mqtt:hum').subscribe((data) => {
       console.log('Humedad: ' + JSON.stringify(data));
+      this.updateChartData(this.chartHumidity, data, 0);
     })
+  }
 
+  updateChartData(chart, data, dataSetIndex) {
+    chart.data.datasets[dataSetIndex].data = data;
+    chart.update();
   }
 }
